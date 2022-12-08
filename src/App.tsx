9bd73a852b951ca500cc00628f737e975bea6758
Haroon Azhar Khan <haroon1azhar@gmail.com>
2022-12-08 21:53:15 +0500

    dependecies added

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,12 @@
 import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import './App.css'
-
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <div className="App">
-      <div>
+      {/* <div>
         <a href="https://vitejs.dev" target="_blank">
           <img src="/vite.svg" className="logo" alt="Vite logo" />
         </a>
@@ -26,7 +25,10 @@ function App() {
       </div>
       <p className="read-the-docs">
         Click on the Vite and React logos to learn more
-      </p>
+      </p> */}
+       <div>
+        <button className='bg-green-200' >okkis</button>
+      </div>
     </div>
   )
 }
